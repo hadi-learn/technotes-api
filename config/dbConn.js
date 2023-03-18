f36e7478e16c84a3763780adb454fe7bdcfe5143
Hadi <hadilearn01@gmail.com>
2023-03-18 12:40:00 +0700

    switch DB to MongoDB Atlas

--- config/dbConn.js
+++ config/dbConn.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose')
 const connectDB = async () => {
   try {
     mongoose.set('strictQuery', true)
-    // await mongoose.connect(process.env.MONGODB_URI)
-    await mongoose.connect('mongodb://127.0.0.1:27017/techNotesDB', {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
+    // await mongoose.connect('mongodb://127.0.0.1:27017/techNotesDB', {
+    //   useNewUrlParser: true,
+    //   useUnifiedTopology: true
+    // })
   } catch (err) {
     console.log(err)
   }
