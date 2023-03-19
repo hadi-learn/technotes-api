2fda0f3877f0cc43b60e53832f0796a0e2292a57
Hadi <hadilearn01@gmail.com>
2023-03-19 20:03:39 +0700

    update to allow onrender deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowerdOrigins = [
   'https://www.technotes.syukri-hadi.com',
-  'https://technotes.syukri-hadi.com'
+  'https://technotes.syukri-hadi.com',
+  'https://technotes-1bs8.onrender.com',
+  'http://localhost:3000'
 ]
 
 module.exports = allowerdOrigins
